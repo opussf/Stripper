f5ae4cd9c03901a6099922ecf33aa84bd6c8cd62
opussf <github@zz9-za.com>
2015-03-07 14:24:57 -0800

    A few renamed tests, 2 new

--- test/test.lua
+++ test/test.lua
@@ -47,26 +47,35 @@ function test.testPlayerIsBusy_EndsFishing()
 	Stripper.COMBAT_TEXT_UPDATE( "SPELL_AURA_REMOVED", "Fishing" ) -- start fishing event
 	assertFalse( Stripper.isBusy )
 end
-function test.test_getFreeBag_HasFreeSpace_OnlyBackpack_Empty()
+function test.testGetFreeBag_HasFreeSpace_OnlyBackpack_Empty()
 	-- Test that it finds a bag that has free space.
 	local bagid = Stripper.getFreeBag()
 	assertEquals( 0, bagid )
 end
-function test.test_Command_Help()
+function test.testCommand_Help()
 	-- Send the help command  -- no side effects to check on
 	Stripper.Command("help")
 end
-function test.test_Command_RemoveOne()
+function test.testCommand_RemoveOne()
 	Stripper.Command("remove")
 end
-function test.test_Command_Default()
+function test.testCommand_Default()
 	Stripper.Command("")
 end
-function test.test_GetItemToRemove_NothingEquipped()
+function test.testCommand_noGear()
+	Stripper.Command("")
+	-- no assert, should do nothing, with no failure.
+end
+function test.testCommand_withGear()
+	myGear[1] = "113596" -- http://us.battle.net/wow/en/item/113596/raid-heroic  -- http://www.wowhead.com/item=113596/vilebreath-mask&bonus=0
+	Stripper.Command("")
+	assertIsNil( myGear[1] ) -- The item should be removed.
+end
+function test.testGetItemToRemove_NothingEquipped()
 	local result = Stripper.getItemToRemove()
 	assertIsNil( result )
 end
-function test.test_GetItemToRemove_HeadEquipped_Number()
+function test.testGetItemToRemove_HeadEquipped_Number()
 	myGear[1] = "113596" -- http://us.battle.net/wow/en/item/113596/raid-heroic  -- http://www.wowhead.com/item=113596/vilebreath-mask&bonus=0
 	local result = Stripper.getItemToRemove()
 	assertEquals( 1, result )
@@ -75,13 +84,6 @@ function test.test_GetItemToRemove_HeadEquipped_Name()
 	myGear[1] = "113596" -- http://us.battle.net/wow/en/item/113596/raid-heroic  -- http://www.wowhead.com/item=113596/vilebreath-mask&bonus=0
 	local result = select(2, Stripper.getItemToRemove())
 	assertEquals( "HeadSlot", result )
-end
-function test.test_Command_noGear()
-	Stripper.Command("")
-end
-function test.test_Command_withGear()
-	myGear[1] = "113596" -- http://us.battle.net/wow/en/item/113596/raid-heroic  -- http://www.wowhead.com/item=113596/vilebreath-mask&bonus=0
-	Stripper.Command("")
 end
 function test.test_RemoveFromSlot()
 end
